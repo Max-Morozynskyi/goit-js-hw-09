cc8ef97ebd2a940a4acae6db8befc040be8b4e05
Max-Morozynskyi <105488461+Max-Morozynskyi@users.noreply.github.com>
2022-11-24 17:12:44 +0000

    Deploying to gh-pages from @ Max-Morozynskyi/goit-js-hw-09@608a3e5e2fa549dd63ea6883636fe920c551e4e0 🚀

---
+++ 01-color-switcher.155257df.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o(),n=setInterval(o,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}));let n;function o(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.disabled=!0;
+//# sourceMappingURL=01-color-switcher.155257df.js.map
